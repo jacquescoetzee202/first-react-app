130979ad670d5dad079b65005a8f7fbbaffe6a38
Jacques <jacquescoetzee91@gmail.com>
2020-11-23 14:29:21 +0000

    added square component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Paragraph from "./components/Paragraph";
+import Square from "./components/Square"
 
 const App = () => (
   <>
     <Header />
     <Paragraph />
+    <Square />
   </>
 );
 
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+const divStyle = {
+    backgroundColor: 'red',
+    height: 200,
+    width: 200
+}
+
+const Square = () => (
+    <div style={divStyle}></div>
+);
+
+export default Square;
