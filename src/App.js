4ee3712195a34ecfbae96a614e54d351aafff9af
Jacques <jacquescoetzee91@gmail.com>
2020-11-26 16:24:03 +0000

    Footer and 404 handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import FourOhFour from "./components/FourOhFour"
 import Adder from "./components/forms/Adder"
 import Length from "./components/forms/Length"
 import List from "./components/forms/List"
@@ -9,29 +10,36 @@ import PasswordStrength from "./components/forms/PasswordStrength"
 import TempConverter from "./components/forms/TempConverter"
 import ToggleText from "./components/state/ToggleText";
 import Die from "./components/stateTricksy/Die";
+import Gif from "./components/Gif";
 
 const App = () => (
   <Router >
     <Header>Hello, world</Header>
-    <Route exact path="/lists">
-      <Adder />
-      <List />
-    </Route>
-    <Route exact path="/form-checker">
-      <Length />
-      <PasswordStrength />
-    </Route>
-    <Route exact path="/temp-converter">
-      <TempConverter />
-    </Route>
-    <Route path="/text-toggle/:target" render={({ match }) => (
-      <ToggleText initial="Hello" alternate={ match.params.target } />
-    )} >
-    </Route>
-    <Route exact path="/die/:sides" render={({ match }) => (
-      <Die sides={ match.params.sides }></Die>
-    )}>
-    </Route>
+    <Switch>
+      <Route exact path="/">
+        <Gif />
+      </Route>
+      <Route exact path="/lists">
+        <Adder />
+        <List />
+      </Route>
+      <Route exact path="/form-checker">
+        <Length />
+        <PasswordStrength />
+      </Route>
+      <Route exact path="/temp-converter">
+        <TempConverter />
+      </Route>
+      <Route path="/text-toggle/:target" render={({ match }) => (
+        <ToggleText initial="Hello" alternate={ match.params.target } />
+      )} >
+      </Route>
+      <Route exact path="/die/:sides" render={({ match }) => (
+        <Die sides={ match.params.sides }></Die>
+      )}>
+      </Route>
+      <FourOhFour />
+    </Switch>
     <Footer />
   </Router>
 );
---
+++ src/components/FourOhFour.js
@@ -0,0 +1,3 @@
+const FourOhFour = () => <p>Page not found : 404</p>;
+
+export default FourOhFour;
